bddd5567eaaa54a47d0af302788c4ceb2b85c6e3
Fernando Carneiro <fernando.silva387@fatec.sp.gov.br>
2023-03-02 15:29:28 -0300

    Adicionando o .gitignore

--- Empregado.java
+++ Empregado.java
@@ -9,14 +9,13 @@ public class Empregado{
     private double bonus;
 
     public double calcularSalario(){
-        if (tipo ==0)
+        if (tipo == 0)
             return salario;
         if (tipo == 1)
             return salario + salario * comissao;
         if (tipo == 2)
             return salario + bonus;
         return 0;
-
     }
 
     public String getNome(){
@@ -31,5 +30,36 @@ public class Empregado{
    }
    public void setIdade(int idade) {
        this.idade = idade;
+   }
+
+   public int getTipo() {
+       return tipo;
+   }
+   public void setTipo(int tipo) {
+       this.tipo = tipo;
+   }
+
+   public double getBonus() {
+       return bonus;
+   }
+
+   public double getComissao() {
+       return comissao;
+   }
+
+   public double getSalario() {
+       return salario;
+   }
+
+   public void setBonus(double bonus) {
+       this.bonus = bonus;
+   }
+
+   public void setComissao(double comissao) {
+       this.comissao = comissao;
+   }
+
+   public void setSalario(double salario) {
+       this.salario = salario;
    }
 }
