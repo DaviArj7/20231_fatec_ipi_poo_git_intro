76d45789625ffa75d18e8a05be6880e2df605621
Fernando Carneiro <fernando.silva387@fatec.sp.gov.br>
2023-03-02 17:42:53 -0300

    Processamento de empregados usando uma lista

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -1,3 +1,4 @@
+import  java.util.ArrayList;
 public class TesteEmpregado {
     public static void main(String[] args) {
         Empregado e1 = new Empregado();
@@ -12,5 +13,19 @@ public class TesteEmpregado {
         e2.setComissao(0.1);
         e3.setSalario(1700);
         e3.setBonus(400);
+
+        var empregados = new ArrayList<Empregado>();
+        empregados.add(e1);
+        empregados.add(e2);
+        empregados.add(e3);
+
+        //enhaced for (Java 5+)
+        /*for(Empregado e : empregados){
+            System.out.println(e.calcularSalario());
+        }*/
+        for(int i=1; i<empregados.size(); i++){
+            System.out.println(empregados.get(i).calcularSalario());
+        }
+        
         }
 }
